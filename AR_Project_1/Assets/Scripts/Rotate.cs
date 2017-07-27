14dd621a4e2a2d9388b818717cb45c4af05e8980
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2017-07-27 13:58:18 -0500

    Subir proyecto
    Proyecto Unity con Vuforia

---
+++ AR_Project_1/Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    //Variable para controlar la velocidad de rotación
+    public int Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Rotar cubo sobre el eje Y
+        transform.Rotate(new Vector3(0, Time.deltaTime * Speed, 0));
+	}
+}
